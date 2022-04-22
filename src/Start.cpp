9aea89f0334e8e8462e56036637f2bc57ff9301a
Emaster5000 <89100747+Emaster5000@users.noreply.github.com>
2022-04-22 12:21:05 -0600

    Update Start.cpp

--- src/Start.cpp
+++ src/Start.cpp
@@ -10,14 +10,14 @@ int main() {
   cout << "██─▄▄▄▄█▄─██─▄█▄─▄▄▀█─▄▄─█▄─█─▄█▄─██─▄█\n";
   cout << "██▄▄▄▄─██─██─███─██─█─██─██─▄▀███─██─██\n";
   cout << "▀▀▄▄▄▄▄▀▀▄▄▄▄▀▀▄▄▄▄▀▀▄▄▄▄▀▄▄▀▄▄▀▀▄▄▄▄▀▀\n\n\n";
-  cout << "Please enter your difficulty, for easy press 1, for medium, 2, for hard, 3, and for expert, 4. If you need help, \nor want to know how to play, press 5.";
+  cout << "Please enter your difficulty, for easy press 1(for end screen), for medium, 2, for hard, 3, and for expert, 4. If you need help, \nor want to know how to play, press 5.\n";
   int dif;
   cin >> dif;
   if(dif == 1) {
-█████████████████████████████████████████████
-█▄─█─▄█─▄▄─█▄─██─▄███▄─█▀▀▀█─▄█▄─▄█▄─▀█▄─▄█░█
-██▄─▄██─██─██─██─█████─█─█─█─███─███─█▄▀─██▄█
-▀▀▄▄▄▀▀▄▄▄▄▀▀▄▄▄▄▀▀▀▀▀▄▄▄▀▄▄▄▀▀▄▄▄▀▄▄▄▀▀▄▄▀▄▀
-
+    cout << "█████████████████████████████████████████████\n";
+    cout << "█▄─█─▄█─▄▄─█▄─██─▄███▄─█▀▀▀█─▄█▄─▄█▄─▀█▄─▄█░█\n";
+    cout << "██▄─▄██─██─██─██─█████─█─█─█─███─███─█▄▀─██▄█\n";
+    cout << "▀▀▄▄▄▀▀▄▄▄▄▀▀▄▄▄▄▀▀▀▀▀▄▄▄▀▄▄▄▀▀▄▄▄▀▄▄▄▀▀▄▄▀▄▀\n";
+    cout << "Thanks for playing, if you wish to play again, restart the application\nDevelopers: Ethan Mabey and Parker Dengg";
   }
 }
